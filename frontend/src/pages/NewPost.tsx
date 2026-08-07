d08f157a887d5cd30c9e20b70738798dbc4b714e
M.Saddique <saddiquem2010@gmail.com>
2026-08-07 06:29:24 +0500

    MVP Done

--- frontend/src/pages/NewPost.tsx
+++ frontend/src/pages/NewPost.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState, useRef } from "react";
 import type { PostType, NewPostType } from "../Types";
+import axios from "axios";
 
 export default function NewPost() {
     const [title, setTitle] = useState("");
@@ -8,14 +9,38 @@ export default function NewPost() {
     const [summary, setSummary] = useState("");
     const [body, setBody] = useState("");
 
-    const [post, setPost] = useState<NewPostType>();
+    const [post] = useState<NewPostType>();
+    const titleRef = useRef(null);
+    const authorRef = useRef(null);
+    const tagRef = useRef(null);
+    const summaryRef = useRef(null);
+    const bodyRef = useRef(null);
 
-
-    const handleSubmit = (e: React.SubmitEvent) => {
+    const handleSubmit = async (e: React.SubmitEvent) => {
         e.preventDefault();
-
-
-        console.log(post);
+        const post = {
+            title: title,
+            author: author,
+            tag: tag,
+            summary: summary,
+            body: body,            
+        }
+        
+        console.log(post); 
+        await axios.post("http://localhost:3000/api/posts/",
+            {
+                title: title,
+                author: author,
+                tag: tag,
+                summary: summary,
+                body: body,
+            });
+        
+        if(titleRef.current) titleRef.current.value = "";
+        if(authorRef.current) authorRef.current.value = "";
+        if(tagRef.current) tagRef.current.value = "";
+        if(summaryRef.current) summaryRef.current.value = "";
+        if(bodyRef.current) bodyRef.current.value = "";
     };
 
     return (
@@ -23,12 +48,12 @@ export default function NewPost() {
             <p>Form</p>
 
             <form onSubmit={handleSubmit}>
-                <input className="border px-2 py-1" type="text" name="title" id="title" placeholder="Title" onChange={(e) => setTitle(e.target.value)} /> <br />
-                <input className="border px-2 py-1" type="text" name="author" id="author" placeholder="Author" onChange={(e) => setAuthor(e.target.value)} /> <br />
-                <input className="border px-2 py-1" type="text" name="tag" id="tag" placeholder="Tag" onChange={(e) => setTag(e.target.value)} /> <br />
-                <input className="border px-2 py-1" type="text" name="summary" id="summary" placeholder="Summary" onChange={(e) => setSummary(e.target.value)} /> <br />
-                <input className="border px-2 py-1" type="text" name="body" id="body" placeholder="Body" onChange={(e) => setBody(e.target.value)} /> <br />
-                <button type="submit">Submit</button>
+                <input ref={titleRef} className="border px-2 py-1" type="text" name="title" id="title" placeholder="Title" onChange={(e) => setTitle(e.target.value)} /> <br />
+                <input ref={authorRef} className="border px-2 py-1" type="text" name="author" id="author" placeholder="Author" onChange={(e) => setAuthor(e.target.value)} /> <br />
+                <input ref={tagRef} className="border px-2 py-1" type="text" name="tag" id="tag" placeholder="Tag" onChange={(e) => setTag(e.target.value)} /> <br />
+                <input ref={summaryRef} className="border px-2 py-1" type="text" name="summary" id="summary" placeholder="Summary" onChange={(e) => setSummary(e.target.value)} /> <br />
+                <input ref={bodyRef} className="border px-2 py-1" type="text" name="body" id="body" placeholder="Body" onChange={(e) => setBody(e.target.value)} /> <br />
+                <button type="submit" className="border">Submit</button>
             </form>
         </div>
     );
--- frontend/src/pages/Posts.tsx
+++ frontend/src/pages/Posts.tsx
@@ -8,17 +8,17 @@ export default function Posts() {
     const [posts, setPosts] = useState<PostType[]>();
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState();
-    // const [refresh, setRefresh] = useState();
+    const [refresh, setRefresh] = useState<number>(0);
 
     useEffect(() => {
         const fetchPosts = async() => {
             await axios.get("http://localhost:3000/api/posts/")
             .then(res => setPosts(res.data))
             .catch((error) => setError(error))
-            .finally(() => setLoading(false)) // runs either way
+            .finally(() => setLoading(false))
         }
         fetchPosts();
-    },[]);
+    },[refresh]);
     
     if (loading) return <p>loading...</p>;
     if (error) return <p>{error}</p>;
@@ -27,7 +27,8 @@ export default function Posts() {
     return (
         <>
             <div>Posts</div>
-            {/* grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 */}
+            <button className="border" onClick={() => setRefresh(refresh + 1)}>Refresh</button>
+
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
                 {posts.map((post) => <PostCard key={post._id} post={post} />)}
             </div>
